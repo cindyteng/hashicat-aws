9d604133440c29a1796c120c76a7dbb93655bab4
root <root@p-pxczeji1sa5u-workstation.c.instruqt-prod.internal>
2021-06-24 09:43:11 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,9 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source  = "app.terraform.io/cindyteng-training/s3-bucket/aws"
 
   bucket_prefix = var.prefix
   acl    = "private"
-
+  version = "2.2.0"
   versioning = {
     enabled = true
   }
